20ded06ccfe195799503e4ec704568f5ee2c5e3a
Edison L <2413125574l@gmail.com>
2026-03-02 14:08:38 +1100

    add relation with label and takitem

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -9,6 +9,14 @@ public class AppDbContext : DbContext
     
     public DbSet<TaskItem>  TaskItems { get; set; }
     public DbSet<Label> Labels { get; set; }
-    
-    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Label 和 TaskItem 一对多关系
+        modelBuilder.Entity<Label>()
+            .HasMany(l => l.TaskItems)       // 一个 Label 有多个 TaskItem
+            .WithOne(t => t.Label)           // 一个 TaskItem 属于一个 Label
+            .HasForeignKey(t => t.LabelId)   // 外键是 LabelId
+            .OnDelete(DeleteBehavior.SetNull); // 删除 Label 时，TaskItem 的 LabelId 设为 null
+    }
 }
--- Migrations/20260302030735_ConfigureRelationship.Designer.cs
+++ Migrations/20260302030735_ConfigureRelationship.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ToDoWebAPI.Data;
 
@@ -11,9 +12,11 @@ using ToDoWebAPI.Data;
 namespace ToDoWebAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260302030735_ConfigureRelationship")]
+    partial class ConfigureRelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
